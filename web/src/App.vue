fc2190f84fdc77d7ea3483445cda6318da66b6b7
yuquanqingyin <1425821304@qq.com>
2025-07-24 15:47:59 +0800

    实现了导航+对战页面的地图和障碍物

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,40 +1,21 @@
 <template>
-  <div>
-    <div>Bot昵称：{{ bot_name }}</div>
-    <div>Bot战力：{{ bot_rating }}</div>
-  </div>
+
+  <NavBar></NavBar>
   <router-view/>
 </template>
 
 <script>
-import { ref } from 'vue';
-import $ from 'jquery';
+import NavBar from '@/components/NavBar.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
 
 
 export default{
   name: "App",
-
-  setup(){
-
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "get",
-      success(resp){
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-
-    });
-
-    return {
-      bot_name,
-      bot_rating,
-    }
-
+  components: {
+    NavBar,
   }
+
 }
 
 
@@ -42,5 +23,10 @@ export default{
 
 
 <style>
+
+body{
+  background-image: url(@/assets/images/background.png);
+  background-size: cover;
+}
 
 </style>
