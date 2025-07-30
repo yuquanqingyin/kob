307fd64ff8581c15114350a86f9ee8e8ed569a98
yuquanqingyin <1425821304@qq.com>
2025-07-30 17:59:15 +0800

    实现微服务：匹配系统（下）-创建matchingsystem微服务并通信

---
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/service/impl/utils/Player.java
@@ -0,0 +1,15 @@
+package com.kob.matchingsystem.service.impl.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+    private Integer userId;
+    private Integer rating;
+    private Integer waitngTime;  //等待时间
+
+}
